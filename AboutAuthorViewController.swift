8c43c4e4a01c0374cc602686a25600e822c3631b
Allen Su <37977270+allensu02@users.noreply.github.com>
2019-07-20 20:39:44 -0700

    Add files via upload

---
+++ AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Julia on 2019/7/20.
+//  Copyright © 2019 Julia. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Julia on 2019/7/19.
+//  Copyright © 2019 Julia. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
