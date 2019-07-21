25f380fa188968bd0bd66a09bcc4287a3ef159b7
Allen Su <37977270+allensu02@users.noreply.github.com>
2019-07-21 11:52:06 -0700

    Add files via upload

---
+++ AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Allen on 2019/7/19.
+//  Copyright © 2019 Allen. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
